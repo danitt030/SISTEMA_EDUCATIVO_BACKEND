066a4365dfbee79f78257429b7e137b97a341fab
danitt030 <danielitotuchez100@gmail.com>
2025-12-17 02:50:26 -0600

    Login y register funcionales para diferentes roles

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,6 +1,7 @@
 import { hash, verify } from "argon2";
 import User from "../user/user.model.js";
 import { generateJWT } from "../helpers/generate-jwt.js";
+import { asignarCodigoPorRole } from "../helpers/generate-codes.js";
 
 export const register = async (req, res) => {
     try {
@@ -10,17 +11,29 @@ export const register = async (req, res) => {
         data.password = encryptedPassword;
         data.profilePicture = profilePicture;
 
-        const user = await User.create(data);
+   
+        const codigosGenerados = await asignarCodigoPorRole(data.role);
+
+        const userData = {
+            ...data,
+            ...codigosGenerados
+        };
+
+        const user = await User.create(userData);
 
         return res.status(201).json({
             success: true,
             message: "Usuario registrado exitosamente",
             user: {
+                uid: user._id,
                 name: user.name,
                 surname: user.surname,
                 email: user.email,
                 role: user.role,
-                profilePicture: user.profilePicture
+                profilePicture: user.profilePicture,
+                ...(user.codigoEstudiante && { codigoEstudiante: user.codigoEstudiante }),
+                ...(user.codigoEmpleado && { codigoEmpleado: user.codigoEmpleado }),
+                ...(user.codigoPadre && { codigoPadre: user.codigoPadre })
             }
         });
     } catch (err) {
@@ -79,6 +92,9 @@ export const login = async (req, res) => {
                 phone: userJson.phone,
                 role: userJson.role,
                 profilePicture: user.profilePicture,
+                ...(userJson.codigoEstudiante && { codigoEstudiante: userJson.codigoEstudiante }),
+                ...(userJson.codigoEmpleado && { codigoEmpleado: userJson.codigoEmpleado }),
+                ...(userJson.codigoPadre && { codigoPadre: userJson.codigoPadre }),
                 token: token
             }
         });
