4e329ffd41cb927878e0af389251c5df03956c47
danitt030 <danielitotuchez100@gmail.com>
2025-12-29 13:07:57 -0600

    Modificacion en conexion a mongo

--- configs/mongo.js
+++ configs/mongo.js
@@ -4,32 +4,39 @@ import mongoose from "mongoose"
 
 export const dbConnection = async () => {
     try{
-        mongoose.connection.on("error", () => {
-            console.log("MongoDB | could not be connect to MongoDB")
+        mongoose.connection.on("error", (err) =>{
+            console.log(" MongoDB | Error:", err)
             mongoose.disconnect()
         })
         mongoose.connection.on("connecting", () =>{
-            console.log("MongoDB | try connecting")
+            console.log(" MongoDB | try connecting")
         })
         mongoose.connection.on("connected", () =>{
-            console.log("MongoDB | connected to MongoDB")
+            console.log(" MongoDB | connected to MongoDB")
         })
         mongoose.connection.on("open", () =>{
-            console.log("MongoDB | Connnected to DataBase")
+            console.log(" MongoDB | Connnected to DataBase")
         })
         mongoose.connection.on("reconnected", () => {
-            console.log("MongoDB | reconnected to MongoDB")
+            console.log(" MongoDB | reconnected to MongoDB")
         })
         mongoose.connection.on("disconnected", () => {
-            console.log("MongoDB | disconnected to MongoDB")
+            console.log(" MongoDB | disconnected to MongoDB")
         })
 
-        await mongoose.connect(process.env.URI_MONGO,{
-            serverSelectionTimeoutMS: 5000,
-            maxPoolSize: 50
+        const connection = await mongoose.connect(process.env.URI_MONGO,{
+            serverSelectionTimeoutMS: 30000, 
+            socketTimeoutMS: 60000,                   
+            maxPoolSize: 50,
+            minPoolSize: 5,
+            maxIdleTimeMS: 30000,
+            connectTimeoutMS: 30000
         })
         
+        return connection;
+        
     }catch(err){
-        console.log(`Database connection failed: ${err}`)
+        console.log(` Database connection failed: ${err}`)
+        throw err; 
     }
 }
